e2c482d9c1892108d22039be363818fefc018d1d
Mario Henze <mario.henze.94@gmail.com>
2021-02-14 15:41:36 +0100

    Implement glslc regexp for lint parsing
    The error_stream needs to be explicitly set to stderr as glslc writes
    linting information only to the error stream.

--- linter.py
+++ linter.py
@@ -1,10 +1,11 @@
 from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import STREAM_STDERR
 
-
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
-    multiline = False
+class glslc(Linter):
+    cmd = ('glslc', '-c', '${file}')
+    regex = r':(?P<line>\d+):\s((?P<warning>warning)|(?P<error>error)):\s\'(?P<near>.*)\'\s:\s+(?P<message>.+)'
+    multiline = True
     defaults = {
-        'selector': 'source.python'
+        'selector': 'source.glsl'
     }
+    error_stream = STREAM_STDERR
