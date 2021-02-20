ad0a04767a9db7565d681b06cea226b6dec0dbcd
Mario Henze <mario.henze.94@gmail.com>
2021-02-20 09:41:52 +0100

    Change to file based linting configuration

--- linter.py
+++ linter.py
@@ -2,10 +2,11 @@ from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLi
 from SublimeLinter.lint import STREAM_STDERR
 
 class glslc(Linter):
-    cmd = ('glslc', '-c', '${file}')
+    cmd = ('glslc', '-c', '${file_on_disk}')
     regex = r':(?P<line>\d+):\s((?P<warning>warning)|(?P<error>error)):\s\'(?P<near>.*)\'\s:\s+(?P<message>.+)'
     multiline = True
     defaults = {
         'selector': 'source.glsl'
     }
+    tempfile_suffix = '-'
     error_stream = STREAM_STDERR
